419990b6bb875cea982fd9e710c4d835c9a4199f
Jeffry Lum <22460123+j-lum@users.noreply.github.com>
2019-07-29 17:14:46 +0800

    Level-0: Rename, Greet, Exit
    Import base branch for students to start off
    
    As student may not have learnt about branching, let's include a copy of
    the base branch into the main repository.
    
    .gitignore: add *.iml
    
    Move Duke.java out of the package
    
    Tweak tutorial text
    
    README: use numbered list for steps
    
    Fix typo in link to checkstyle config files
    
    Update list of contributors
    
    Add a tutorial on text UI testing
    
    textUiTestingTutorial.md: add missing <br>
    
    textUiTestingTutorial.md: mention updating java/javac commands
    
    Specify checkstyle version
    
    Not specifying the checkstyle version causes it to fail with a cryptic
    error.
    
    Adapt JavaFX tutorial for Duke
    
    Rather than to have distinct tutorials building up to a Duke-like
    application, let's merge the tutorials into Duke's tutorials
    to enhance cohesiveness in the course material.
    
    We also merge the gradle/gradleless tutorials into one by providing a
    universal entry point to JavaFX in the style of AddressBook
    applications.
    
    Fix header levels in JavaFX Tutorial 1.
    
    Change code samples to use Duke.
    
    Add hints on required import statements.
    
    Add location hints to code snippets to help students find where to copy
    and paste them.
    
    Remove nitpicks to make the development process smoother.
    
    Fix usage of `Collections` to `FXCollections`.
    
    Replace image for JavaFX tutorial 3.
    
    Specify location to place images.
    
    Replace a screenshot that referred to the outdated package structure.
    
    Remove reference to DukeStub.
    
    Update Gradle tutorial to reflect new checkstyle config
    
    Fix typo referring to `HelloWorld`
    
    Add images to JavaFX tutorial 3
    
    Remove references to DukeStub
    
    Change indentation in FXML samples to match AB-3
    
    JavaFX tutorial: Support cross-platform JARs
    
    The OpenJFX plugin expects applications to be modular and bundled
    with jlink, resulting in fat jars that are not cross-platform. Let's
    manually include the required dependencies so that shadow can package
    them properly.
    
    Remove unnecessary override annotation
    
    Remove extra '@Override' at the start of the code example.
    
    Remove generic tutorials
    
    Let's remove generic tutorials from this repo as they are not specific
    to this project tempalte
    
    Add text-ui-test files
    
    README.md: Fix typo of => or
    
    Improve runtest.* scripts
    
    * Improve the javac command to work for multiple java files.
    * In runtest.bat, delete ACTUAL.TXT only if it exists.
    
    README: Update setup instructions to match the latest IDE version
    
    README.md: Tweak template text
    
    * Let's replace 'Feature 1' with 'Feature-ABC'
      reason: some students kept the 'Feature 1' in the final UG.
    * Let's tweak the template for features and usage
      reasons: to fix grammar, to distinguish between output and outcome
    
    README: Mention the project language level setting
    
    .gitignore: Fix ACTUAL.txt -> ACTUAL.TXT  (#73)
    
    text-ui-test/runtest.bat|sh scripts generate a file ACTUAL.TXT.
    However, .gitignore uses ACTUAL.txt, which means the generated
    file will not be ignored by Git on non-Windows OS.
    
    Let's update .gitignore as ACTUAL.txt -> ACTUAL.TXT
    
    docs/README.md: Tweak document template
    
    Let's tweak the docs/README.md (which is used as the user guide)
    to fit Duke better. Specifically,
    
    1. mention product name in the title
    2. mention adding a product screenshot and a product intro
    3. tweak the flow to describe feature-by-feature
    
    README: Update the JDK version 11 -> 17
    
    README: Ask to keep source folder structure
    
    README: Use correct item number in list
    
    Level-0: Rename, Greet, Exit

---
+++ jav.bat
@@ -0,0 +1,4 @@
+@echo off
+javac C:\Users\xiang\duke\src\main\java\StarPlatinum.java
+java C:\Users\xiang\duke\src\main\java\StarPlatinum.java
+pause
---
+++ src/main/java/Duke.java
@@ -0,0 +1,25 @@
+public class StarPlatinum {
+
+    public static void main(String[] args) {
+        String logo = "      _                     _       _   _                       \r\n"
+                + "  ___| |_ __ _ _ __   _ __ | | __ _| |_(_)_ __  _   _ _ __ ___  \r\n"
+                + " / __| __/ _` | '__| | '_ \\| |/ _` | __| | '_ \\| | | | '_ ` _ \\ \r\n"
+                + " \\__ \\ || (_| | |    | |_) | | (_| | |_| | | | | |_| | | | | | |\r\n"
+                + " |___/\\__\\__,_|_|    | .__/|_|\\__,_|\\__|_|_| |_|\\__,_|_| |_| |_|\r\n"
+                + "                     |_|                                        ";
+
+        System.out.println("Hello from\n" + logo);
+
+        String greeting = "____________________________________________________________\n"
+                + "Hello! I'm Star Platinum\n"
+                + "What can I do for you?\n"
+                + "____________________________________________________________";
+
+        String farewell = "____________________________________________________________\n"
+                + "Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________";
+
+        System.out.println(greeting);
+        System.out.println(farewell);
+    }
+}
