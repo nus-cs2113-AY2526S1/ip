5f2b3c8d6a2df408cbe652c3e21b2c55c7887d64
k1b1t0 <nguyenminhduc2882005@gmail.com>
2025-08-17 07:11:20 +0800

    Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,15 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = " ____                       ___    _       _ \n"
+                + "/ ___| _   _ _ __   ___ _ _|_ _|__| | ___ | |\n"
+                + "\\___ \\| | | | '_ \\ / _ \\ '__| |/ _` |/ _ \\| |\n"
+                + " ___) | |_| | |_) |  __/ |  | | (_| | (_) | |\n"
+                + "|____/ \\__,_| .__/ \\___|_| |___\\__,_|\\___/|_|\n"
+                + "            |_|                              \n";
+        System.out.println("Hello! I'm\n" + logo);
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
     }
 }
