13c6af572af55f3d3a9defa780d0ae0dfef579f2
Huanghao <andrewxu1688@gmail.com>
2025-08-22 15:48:49 +0800

    Level 0: Rename, Greet, Exit

---
+++ src/main/java/Nova.java
@@ -0,0 +1,5 @@
+public class Nova {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________\nHello! I'm Nova\nWhat can I do for you?\n____________________________________________________________\nBye. Hope to see you again soon!\n____________________________________________________________\n");
+    }
+}
