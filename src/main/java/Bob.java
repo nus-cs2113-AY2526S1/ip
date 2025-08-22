0a9318be8259a39e33bb6fc6ae00f1dc7817678f
enwei <enwei2929@gmail.com>
2025-08-22 14:41:19 +0800

    Complete Level-0 - rename Duke to Bob, add greeting and exit flow

---
+++ src/main/java/Bob.java
@@ -0,0 +1,18 @@
+
+public class Bob {
+
+    public static void main(String[] args) {
+        String logo
+                = " ____   ___   ____  \n"
+                + "| __ ) / _ \\ | __ ) \n"
+                + "|  _ \\| | | ||  _ \\ \n"
+                + "| |_) | |_| || |_) |\n"
+                + "|____/ \\___/ |____/ \n";
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello from\n" + logo);
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
