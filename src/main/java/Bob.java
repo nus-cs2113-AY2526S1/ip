54012bff1e5674837441dae7bcf7f18e17069371
enwei <enwei2929@gmail.com>
2025-08-25 17:41:32 +0800

    Implement echo loop and terminate on "bye"

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -1,18 +1,35 @@
 
+import java.util.Scanner;
 public class Bob {
 
     public static void main(String[] args) {
-        String logo
-                = " ____   ___   ____  \n"
-                + "| __ ) / _ \\ | __ ) \n"
-                + "|  _ \\| | | ||  _ \\ \n"
-                + "| |_) | |_| || |_) |\n"
-                + "|____/ \\___/ |____/ \n";
-        System.out.println("____________________________________________________________");
+        String logo =
+                """
+                         ____   ___   ____ \s
+                        | __ ) / _ \\ | __ )\s
+                        |  _ \\| | | ||  _ \\\s
+                        | |_) | |_| || |_) |
+                        |____/ \\___/ |____/\s
+                        """;
+
+        System.out.println();
         System.out.println("Hello from\n" + logo);
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        boolean endConvo = false;
+        while (!endConvo) {
+            Scanner scanner = new Scanner(System.in);
+            String sentence = scanner.nextLine();
+            if (!sentence.equals("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println(sentence);
+                System.out.println("____________________________________________________________");
+            } else {
+                endConvo = true;
+                System.out.println("____________________________________________________________");
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+            }
+        }
     }
 }
