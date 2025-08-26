842115aef72cb1afb52f9519f94c3672d8f56717
k1b1t0 <nguyenminhduc2882005@gmail.com>
2025-08-26 14:23:04 +0800

    Add to do list and function add to list

--- src/main/java/SuperIdol.java
+++ src/main/java/SuperIdol.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
 
 public class SuperIdol {
+
+    private static String[] toDoList = new String[100];
+    private static int listCount = 0;
+
     public static void main(String[] args) {
         String logo = " ____                       ___    _       _ \n"
                 + "/ ___| _   _ _ __   ___ _ _|_ _|__| | ___ | |\n"
@@ -18,8 +22,11 @@ public class SuperIdol {
                 talk("Bye. Hope to see you again soon!");
                 System.exit(0);
             }
+            else if (command.equals("list")) {
+                showList();
+            }
             else {
-                talk(command);
+                addToList(command);
             }
         }
     }
@@ -27,6 +34,20 @@ public class SuperIdol {
     public static void talk(String response) {
         System.out.println("____________________________________________________________");
         System.out.println(response);
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void addToList(String command) {
+        toDoList[listCount] = command;
+        listCount++;
+        talk("added: " + command);
+    }
+
+    public static void showList() {
+        System.out.println("____________________________________________________________");
+        for (int i = 0; i < listCount; i++) {
+            System.out.println((i+1) + ". " + toDoList[i]);
+        }
         System.out.println("____________________________________________________________");
     }
 }
