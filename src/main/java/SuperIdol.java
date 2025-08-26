7550ca98746ae7c1e9451b8d3c09d3bfa16779b3
k1b1t0 <nguyenminhduc2882005@gmail.com>
2025-08-26 14:09:50 +0800

    Add echo for chatbot

--- src/main/java/SuperIdol.java
+++ src/main/java/SuperIdol.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class SuperIdol {
     public static void main(String[] args) {
         String logo = " ____                       ___    _       _ \n"
@@ -6,10 +8,25 @@ public class SuperIdol {
                 + " ___) | |_| | |_) |  __/ |  | | (_| | (_) | |\n"
                 + "|____/ \\__,_| .__/ \\___|_| |___\\__,_|\\___/|_|\n"
                 + "            |_|                              \n";
-        System.out.println("Hello! I'm\n" + logo);
+        System.out.println("Hello! I'm SuperIdol\n" + logo);
         System.out.println("What can I do for you?");
+
+        Scanner in = new Scanner(System.in);
+        while (true) {
+            String command =  in.nextLine();
+            if (command.equals("exit")) {
+                talk("Bye. Hope to see you again soon!");
+                System.exit(0);
+            }
+            else {
+                talk(command);
+            }
+        }
+    }
+
+    public static void talk(String response) {
         System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(response);
         System.out.println("____________________________________________________________");
     }
 }
