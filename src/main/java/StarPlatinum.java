c79a37c50163ab4a4d04ee074f964ad333471b98
yx <yx@email.com>
2025-08-29 18:41:23 +0800

    Level-1: Echo functionality

--- src/main/java/StarPlatinum.java
+++ src/main/java/StarPlatinum.java
@@ -1,25 +1,42 @@
+
+import java.util.Scanner;
+
 public class StarPlatinum {
+        public static void main(String[] args) {
+                String logo = "      _                     _       _   _                       \r\n" + //
+                                "  ___| |_ __ _ _ __   _ __ | | __ _| |_(_)_ __  _   _ _ __ ___  \r\n" + //
+                                " / __| __/ _` | '__| | '_ \\| |/ _` | __| | '_ \\| | | | '_ ` _ \\ \r\n" + //
+                                " \\__ \\ || (_| | |    | |_) | | (_| | |_| | | | | |_| | | | | | |\r\n" + //
+                                " |___/\\__\\__,_|_|    | .__/|_|\\__,_|\\__|_|_| |_|\\__,_|_| |_| |_|\r\n" + //
+                                "                     |_|                                        ";
 
-    public static void main(String[] args) {
-        String logo = "      _                     _       _   _                       \r\n"
-                + "  ___| |_ __ _ _ __   _ __ | | __ _| |_(_)_ __  _   _ _ __ ___  \r\n"
-                + " / __| __/ _` | '__| | '_ \\| |/ _` | __| | '_ \\| | | | '_ ` _ \\ \r\n"
-                + " \\__ \\ || (_| | |    | |_) | | (_| | |_| | | | | |_| | | | | | |\r\n"
-                + " |___/\\__\\__,_|_|    | .__/|_|\\__,_|\\__|_|_| |_|\\__,_|_| |_| |_|\r\n"
-                + "                     |_|                                        ";
+                System.out.println("Hello from\n" + logo);
 
-        System.out.println("Hello from\n" + logo);
+                String greeting = "____________________________________________________________\n" +
+                                "ORA ORA ORA ORA ORA!\n" +
+                                "Star Platinum is here — what can I do for you?\n" +
+                                "____________________________________________________________";
 
-        String greeting = "____________________________________________________________\n"
-                + " Hello! I'm Star Platinum\n"
-                + " What can I do for you?\n"
-                + "____________________________________________________________";
+                String farewell = "____________________________________________________________\n" +
+                                "Yare Yare Daze... Star Platinum will see you again.\n" +
+                                "____________________________________________________________";
 
-        String farewell = "____________________________________________________________\n"
-                + " Bye. Hope to see you again soon!\n"
-                + "____________________________________________________________";
+                System.out.println(greeting + "\n");
 
-        System.out.println(greeting);
-        System.out.println(farewell);
-    }
+                Scanner scanner = new Scanner(System.in);
+
+                String userInput = "";
+                while (!userInput.equals("bye")) {
+                        userInput = scanner.nextLine();
+
+                        if (!userInput.equals("bye")) {
+                                System.out.println("____________________________________________________________");
+                                System.out.println(userInput);
+                                System.out.println("____________________________________________________________\n");
+                        }
+                }
+
+                scanner.close();
+                System.out.println(farewell);
+        }
 }
