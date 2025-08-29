1fae9bbeb228cf9f70180972e22514975782a7f2
Huanghao <andrewxu1688@gmail.com>
2025-08-29 13:37:45 +0800

    added echo functionality and exit command for Nova chatbot

--- src/main/java/Nova.java
+++ src/main/java/Nova.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class Nova {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________\nHello! I'm Nova\nWhat can I do for you?\n____________________________________________________________\nBye. Hope to see you again soon!\n____________________________________________________________\n");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("""
+                ____________________________________________________________
+                Hello! I'm Nova
+                What can I do for you?
+                ____________________________________________________________
+                """);
+
+        while (true) {
+            String userInput = scanner.nextLine();
+            if (userInput.equals("bye")) {
+                System.out.println("""
+                        ____________________________________________________________
+                         Bye. Hope to see you again soon!
+                        ____________________________________________________________
+                        """);
+                break;
+            }
+            System.out.println("____________________________________________________________");
+            System.out.println(" " + userInput);
+            System.out.println("___________________________________________________________");
+        }
     }
 }
