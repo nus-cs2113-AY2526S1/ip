60b0816a057de9ab138824ecc6367ad0618ca512
yx <yx@email.com>
2025-08-29 18:43:59 +0800

    Level-3: Mark as Done

--- src/main/java/StarPlatinum.java
+++ src/main/java/StarPlatinum.java
@@ -2,6 +2,28 @@ import java.util.Scanner;
 import java.util.Vector;
 
 public class StarPlatinum {
+        public static class Task {
+                protected String description;
+                protected boolean isDone;
+
+                public Task(String description) {
+                        this.description = description;
+                        isDone = false;
+                }
+
+                public void mark() {
+                        this.isDone = true;
+                }
+
+                void unmark() {
+                        this.isDone = false;
+                }
+
+                public String toString() {
+                        return (isDone ? "[X]" : "[ ]") + description;
+                }
+        }
+
         public static void main(String[] args) {
                 String logo = "      _                     _       _   _                       \r\n" + //
                                 "  ___| |_ __ _ _ __   _ __ | | __ _| |_(_)_ __  _   _ _ __ ___  \r\n" + //
@@ -13,42 +35,90 @@ public class StarPlatinum {
                 System.out.println("Hello from\n" + logo);
 
                 String greeting = "____________________________________________________________\n" +
-                                "ORA ORA ORA ORA ORA!\n" +
-                                "Star Platinum is here, what can I do for you?\n" +
+                                " ORA ORA ORA ORA ORA!\n" +
+                                " Star Platinum is here, what can I do for you?\n" +
                                 "____________________________________________________________";
 
                 String farewell = "____________________________________________________________\n" +
-                                "Yare Yare Daze... Star Platinum will see you again.\n" +
+                                " Yare Yare Daze... Star Platinum will see you again.\n" +
                                 "____________________________________________________________";
 
                 System.out.println(greeting + "\n");
 
-                Vector<String> tasks = new Vector<>();
-                Scanner scanner = new Scanner(System.in);
+                Vector<Task> storage = new Vector<>();
+
+                Scanner scanner = new Scanner(System.in); // scans constantly;
 
                 String userInput = "";
                 while (!userInput.equals("bye")) {
-                        userInput = scanner.nextLine();
+                        userInput = scanner.nextLine(); // gets user input
+                        String[] parts = userInput.split(" "); // ["mark", "2"]
+                        String command = parts[0]; // "mark"
+                        int taskNumber = -1; // default value
 
-                        if (userInput.equals("list")) {
-                                System.out.println("____________________________________________________________");
-                                if (tasks.isEmpty()) {
-                                        System.out.println("Your task list is empty!");
-                                } else {
-                                        for (int i = 0; i < tasks.size(); i++) {
-                                                System.out.println(" " + (i + 1) + ". " + tasks.get(i));
+                        if ((command.equals("mark") || command.equals("unmark")) && parts.length >= 2) {
+                                taskNumber = Integer.parseInt(parts[1]); // only now safe to parse
+                        }
+
+                        if (command.equals("list")) {
+                                int i = 0;
+                                System.out.println("Zaaaa Woruuudo");
+                                while (i < storage.size()) {
+                                        System.out.println((i + 1) + "." + storage.get(i));
+                                        i++;
+                                }
+                                System.out.println('\n');
+                        } else if (command.equals("mark") || command.equals("unmark")) {
+                                if (parts.length < 2) {
+                                        System.out.println("Which task number would you like to " + command + "?");
+                                        // list
+                                        int i = 0;
+                                        System.out.println("Zaaaa Woruuudo");
+                                        while (i < storage.size()) {
+                                                System.out.println((i + 1) + "." + storage.get(i));
+                                                i++;
+                                        }
+                                        System.out.println('\n');
+
+                                        // get task number
+                                        boolean valid = false;
+
+                                        while (!valid) {
+                                                System.out.println("Please enter a valid task number:");
+
+                                                if (scanner.hasNextInt()) {
+                                                        taskNumber = scanner.nextInt();
+                                                        scanner.nextLine(); // consume leftover newline
+
+                                                        if (taskNumber >= 1 && taskNumber <= storage.size()) {
+                                                                valid = true; // success, exit loop
+                                                        } else {
+                                                                System.out.println("Number out of range!");
+                                                        }
+                                                } else {
+                                                        System.out.println("That's not a number!");
+                                                        scanner.nextLine(); // consume the invalid input
+                                                }
                                         }
                                 }
-                                System.out.println("____________________________________________________________\n");
-                        } else if (!userInput.equals("bye")) {
-                                tasks.add(userInput);
-                                System.out.println("____________________________________________________________");
-                                System.out.println("added: " + userInput);
-                                System.out.println("____________________________________________________________\n");
+                                if (taskNumber > 0 && taskNumber <= storage.size()) {
+                                        if (command.equals("mark")) {
+                                                storage.get(taskNumber - 1).mark();
+                                                System.out.println("Marked task: " + storage.get(taskNumber - 1));
+                                        } else {
+                                                storage.get(taskNumber - 1).unmark();
+                                                System.out.println("Unmarked task: " + storage.get(taskNumber - 1));
+                                        }
+                                } else {
+                                        System.out.println("Invalid task number.");
+                                }
+                                System.out.println('\n');
+                        } else if (!command.equals("bye") && taskNumber == -1) {
+                                storage.add(new Task(userInput));
+                                System.out.println("added: " + storage.lastElement() + "\n");
                         }
                 }
-
-                scanner.close();
+                scanner.close(); // close scanner
                 System.out.println(farewell);
         }
 }
