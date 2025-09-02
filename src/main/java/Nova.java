1eb7444c8d360f1af1e7621fa90d7c097be1ff7c
Huanghao <andrewxu1688@gmail.com>
2025-09-03 01:18:36 +0800

    added support for Todos, Deadlines, and Events using inheritance

--- src/main/java/Nova.java
+++ src/main/java/Nova.java
@@ -48,6 +48,26 @@ public class Nova {
                 System.out.println(" OK, I've maked this task as not done yet:");
                 System.out.println("    " + t);
                 System.out.println("____________________________________________________________");
+            } else if (userInput.startsWith("todo")) {
+                String taskDescription = userInput.substring(5);
+                Task newTask = new Todo(taskDescription);
+                tasks[taskCount++] = newTask;
+                printTaskAdded(newTask, taskCount);
+            } else if (userInput.startsWith("deadline")) {
+                String[] parts = userInput.split("/by");
+                String taskDescription = parts[0].substring(9).trim();
+                String by = parts[1].trim();
+                Task newTask = new Deadline(taskDescription, by);
+                tasks[taskCount++] = newTask;
+                printTaskAdded(newTask, taskCount);
+            } else if (userInput.startsWith("event")) {
+                String[] parts = userInput.split("/from|/to");
+                String taskDescription = parts[0].substring(6).trim();
+                String from = parts[1].trim();
+                String to = parts[2].trim();
+                Task newTask = new Event(taskDescription, from, to);
+                tasks[taskCount++] = newTask;
+                printTaskAdded(newTask, taskCount);
             } else {
                 Task newTask = new Task(userInput);
                 tasks[taskCount] = newTask;
@@ -57,6 +77,14 @@ public class Nova {
                 System.out.println("___________________________________________________________");
             }
         }
+    }
+
+    public static void printTaskAdded(Task task, int taskCount) {
+        System.out.println("____________________________________________________________");
+        System.out.println(" Got it. I've added this task:");
+        System.out.println("    " + task);
+        System.out.println(" Now you have " + taskCount + " tasks in the list");
+        System.out.println("____________________________________________________________");
     }
 }
 
