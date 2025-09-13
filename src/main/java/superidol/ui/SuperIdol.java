cd99a086e1751343b087dc9a8981903256e156ee
k1b1t0 <nguyenminhduc2882005@gmail.com>
2025-09-13 10:25:46 +0800

    Add delete feature

--- src/main/java/superidol/ui/SuperIdol.java
+++ src/main/java/superidol/ui/SuperIdol.java
@@ -5,13 +5,14 @@ import superidol.task.Event;
 import superidol.task.Task;
 import superidol.task.Todo;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class SuperIdol {
 
-    private static Task[] toDoList = new Task[100];
+    private static ArrayList<Task> toDoList = new ArrayList<>();
 
     public static void main(String[] args) {
         greeting();
@@ -44,6 +45,9 @@ public class SuperIdol {
                     break;
                 case "event":
                     addEvent(command);
+                    break;
+                case "delete":
+                    deleteTask(command);
                     break;
                 default:
                     reponse();
@@ -114,6 +118,7 @@ public class SuperIdol {
             talk("Wrong input! Try \"event <task> /from <start> /to <end>\"");
         }
     }
+
     public static void addTodo(String command) {
         // remove "todo"
         String task = command.substring(4).trim();
@@ -125,20 +130,15 @@ public class SuperIdol {
         }
     }
 
-    public static void addTask(String command) {
-        Task newTask = new Task(command);
-        addToList(newTask, command);
-    }
-
     public static void addToList(Task newTask, String task) {
         // add task to the list
-        toDoList[Task.taskCount] = newTask;
+        toDoList.add(newTask);
         Task.taskCount++;
 
         // print the result
         System.out.println("____________________________________________________________");
         System.out.println("Got it. I've added this task:");
-        System.out.println(toDoList[Task.taskCount - 1].getTask());
+        System.out.println(newTask.getTask());
         System.out.println("Now you have " + Task.taskCount + " tasks in the list.");
         System.out.println("____________________________________________________________");
     }
@@ -149,7 +149,7 @@ public class SuperIdol {
         } else {
             System.out.println("____________________________________________________________");
             for (int i = 0; i < Task.taskCount; i++) {
-                System.out.println((i + 1) + ". " + toDoList[i].getTask());
+                System.out.println((i + 1) + ". " + toDoList.get(i).getTask());
             }
             System.out.println("____________________________________________________________");
         }
@@ -159,13 +159,14 @@ public class SuperIdol {
         try {
             int taskId = Integer.parseInt(command.split(" ")[1]);
             if (taskId >= 1 && taskId <= Task.taskCount) {
-                toDoList[taskId - 1].setIsDone(isDone);
+                Task targetTask = toDoList.get(taskId - 1);
+                targetTask.setIsDone(isDone);
                 if (isDone) {
                     talk("Nice! I've marked this task as done:\n"
-                            + toDoList[taskId - 1].getTask());
+                            + targetTask.getTask());
                 } else {
                     talk("OK, I've marked this task as not done yet:\n"
-                            + toDoList[taskId - 1].getTask());
+                            + targetTask.getTask());
                 }
             } else {
                 talk("You only have " + Task.taskCount + " task(s) in the list.");
@@ -173,6 +174,22 @@ public class SuperIdol {
         } catch (NumberFormatException e) {
             talk("Wrong input! Try \"(un)mark <task_index>\"");
         }
+    }
 
+    public static void deleteTask(String command) {
+        try {
+            int taskId = Integer.parseInt(command.split(" ")[1]);
+            if (taskId >= 1 && taskId <= Task.taskCount) {
+                talk("Noted. I've removed this task:\n"
+                        + toDoList.get(taskId - 1).getTask()
+                        + "\nNow you have " + (Task.taskCount - 1) + " tasks in the list.");
+                toDoList.remove(taskId - 1);
+                Task.taskCount--;
+            } else {
+                talk("You only have " + Task.taskCount + " task(s) in the list.");
+            }
+        } catch (NumberFormatException e) {
+            talk("Wrong input! Try \"delete <task_index>\"");
+        }
     }
 }
