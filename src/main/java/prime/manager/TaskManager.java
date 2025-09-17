74ab25d9b6308c382017b6fadfaf24299257fcec
T. Kandasami <t.kandasami1@gmail.com>
2025-09-17 13:41:27 +0800

    Update TaskManager to handle the Storage methods

--- src/main/java/prime/manager/TaskManager.java
+++ src/main/java/prime/manager/TaskManager.java
@@ -2,43 +2,53 @@ package prime.manager;
 
 import prime.exceptions.InvalidTaskNumberException;
 import prime.exceptions.PrimeException;
+import prime.storage.Storage;
 import prime.task.Task;
 import prime.ui.UserInterface;
 
+import java.util.ArrayList;
+
 public class TaskManager {
     private static final int MAX_TASKS = 100;
-    private final Task[] tasks = new Task[MAX_TASKS];
-    private int taskCounter = 0;
+    private final ArrayList<Task> tasks;
+    private final Storage storage;
+
+    public TaskManager() {
+        this.storage = new Storage();
+        this.tasks = new ArrayList<>(storage.load());
+    }
 
     public void addTask(Task task, UserInterface ui) throws PrimeException {
-        if (taskCounter < MAX_TASKS) {
-            tasks[taskCounter] = task;
+        if (tasks.size() < MAX_TASKS) {
+            tasks.add(task);
+            storage.save(tasks);
             ui.printIndented("Got it. I've added this task:");
             ui.printIndented(task.toString());
-            taskCounter++;
-            ui.printIndented("Now you have " + taskCounter + " tasks in your task list.");
+            ui.printIndented("Now you have " + tasks.size() + " tasks in your task list.");
         } else {
             throw new PrimeException("Task list is full Human! Maybe it’s time you finish some tasks.");
         }
     }
 
     public void listTasks(UserInterface ui) {
-        if (taskCounter == 0) {
+        if (tasks.isEmpty()) {
             ui.printIndented("No tasks have been added in your list yet.");
             return;
         }
         ui.printIndented("Here are your tasks in your list:");
-        for (int i = 0; i < taskCounter; i++) {
-            ui.printIndented((i + 1) + "." + tasks[i]);
+        for (int i = 0; i < tasks.size(); i++) {
+            ui.printIndented((i + 1) + "." + tasks.get(i));
         }
     }
 
     public void toggleMark(int taskNo, UserInterface ui) throws InvalidTaskNumberException {
-        if (taskNo < 1 || taskNo > taskCounter) {
-            throw new InvalidTaskNumberException(taskNo,taskCounter);
+        if (taskNo < 1 || taskNo > tasks.size()) {
+            throw new InvalidTaskNumberException(taskNo,tasks.size());
         }
-        Task task = tasks[taskNo - 1];
+        Task task = tasks.get(taskNo - 1);
         task.setIsDone(!task.getIsDone());
+        storage.save(tasks);
+
         if (task.getIsDone()) {
             ui.printIndented("Nice Human! I've marked this task as done:");
         } else {
