f5b0905c5ef49ba202a0bfd8a62d94f49d4f614c
Huanghao <andrewxu1688@gmail.com>
2025-09-27 00:21:34 +0800

    Added JavaDoc comments to classes and methods in Nova

--- src/main/java/Nova/command/AddDeadlineCommand.java
+++ src/main/java/Nova/command/AddDeadlineCommand.java
@@ -6,13 +6,34 @@ import Nova.task.Deadline;
 import Nova.task.TaskList;
 import Nova.ui.TextUi;
 
+/**
+ * Represents a command to add a Deadline task to the task list.
+ * The command parses user input to create a Deadline with a description
+ * and a due date/time.
+ */
 public class AddDeadlineCommand extends Command {
     private final String arguments;
 
+    /**
+     * Constructs an AddDeadlineCommand with the specified user input.
+     *
+     * @param arguments The full arguments string provided by the user
+     *                  in the format: "<description> /by <date-time>".
+     */
     public AddDeadlineCommand(String arguments) {
         this.arguments = arguments;
     }
 
+    /**
+     * Executes the command by creating a new Deadline task from the arguments
+     * and adding it to TaskList, displaying it by the UI, and saving the
+     * updated list to Storage.
+     *
+     * @param tasks The TaskList containing the current tasks.
+     * @param ui The TextUi instance for displaying messages to the user.
+     * @param storage The Storage instance for saving/loading the updated task list.
+     * @throws NovaException If the arguments are invalid or in the wrong format.
+     */
     @Override
     public void execute(TaskList tasks, TextUi ui, Storage storage) throws NovaException {
         try {
--- src/main/java/Nova/parser/Parser.java
+++ src/main/java/Nova/parser/Parser.java
@@ -14,7 +14,24 @@ import Nova.command.HelpCommand;
 
 import Nova.exception.NovaException;
 
+
+/**
+ * The {@code Parser} class is responsible for interpreting raw user input
+ * and converting it into an executable {@link Command}.
+ * <p>
+ * It splits the input into the command keyword and its arguments,
+ * then maps the keyword to the corresponding {@code Command} object.
+ */
 public class Parser {
+
+    /**
+     * Parses user input into a {@link Command}.
+     *
+     * @param userInput The full string input entered by the user.
+     * @return A {@link Command} object corresponding to the user input.
+     * @throws NovaException If the command is unrecognized or arguments are invalid.
+     */
+
     public static Command parseCommand(String userInput) throws NovaException {
         String[] inputArray = userInput.split(" ", 2);
         String commandWord = inputArray[0];
