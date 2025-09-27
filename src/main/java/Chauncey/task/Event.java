231486ec4ef5c36c65a6fd494202e7f8b5735243
Kennahh <peishenhe@gmail.com>
2025-09-27 20:32:56 +0800

    store deadline, start time and end time as LocalDateTime objects

--- src/main/java/Chauncey/task/Event.java
+++ src/main/java/Chauncey/task/Event.java
@@ -1,37 +1,51 @@
 package Chauncey.task;
 
-public class Event extends Task{
-    private String startTime;
-    private String endTime;
-    private static final char LABEL = 'E';
+import Chauncey.exception.ChaunceyException;
 
-    public Event(String description, String startTime, String endTime) {
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Locale;
+
+public class Event extends Task{
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private static final char LABEL = 'E';
+    private static final DateTimeFormatter INPUT_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm", Locale.US);
+    private static final DateTimeFormatter OUTPUT_FORMATTER = DateTimeFormatter.ofPattern("MMM dd yyyy HH:mm", Locale.US);
+
+    public Event(String description, String startTime, String endTime) throws DateTimeParseException, ChaunceyException {
         super(description);
-        this.startTime = startTime;
-        this.endTime = endTime;
+        if (!startTime.contains("from") || !endTime.contains("to")) {
+            throw new ChaunceyException("Please follow the input details format: <description> / from <yyyy-MM-dd HHmm> / to <yyyy-MM-dd HHmm>.");
+        }
+        String startTimeDate = startTime.substring(5);
+        String endTimeDate = endTime.substring(3);
+        this.startTime = LocalDateTime.parse(startTimeDate, INPUT_FORMATTER);
+        this.endTime = LocalDateTime.parse(endTimeDate, INPUT_FORMATTER);
     }
 
     public String getStartTime() {
-        return startTime;
+        return startTime.format(OUTPUT_FORMATTER);
     }
 
     public String getEndTime() {
-        return endTime;
+        return endTime.format(OUTPUT_FORMATTER);
     }
 
     @Override
     public void outputTaskDetails() {
-        System.out.println("[" + LABEL + "][" + super.getStatusIcon() + "] " + super.getDescription() + " (" + startTime + " " + endTime + ")");
+        System.out.println("[" + LABEL + "][" + super.getStatusIcon() + "] " + super.getDescription() + " (from " + getStartTime() + " to " + getEndTime() + ")");
     }
 
     @Override
     public String getTaskDetails() {
-        return "[" + LABEL + "][" + super.getStatusIcon() + "] " + super.getDescription() + " (" + startTime + " " + endTime + ")";
+        return "[" + LABEL + "][" + super.getStatusIcon() + "] " + super.getDescription() + " (from " + getStartTime() + " to " + getEndTime() + ")";
     }
 
     @Override
     public String writeToFile() {
         int isDoneInInteger = getStatus()? 1 : 0;
-        return LABEL + " | " + isDoneInInteger + " | " + getDescription() + " | " + startTime + " | " + endTime;
+        return LABEL + " | " + isDoneInInteger + " | " + getDescription() + " | from " + startTime.format(INPUT_FORMATTER) + " | to " + endTime.format(INPUT_FORMATTER);
     }
 }
